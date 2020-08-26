a173db219921a39a9ffd4788284866f624c3aa54
jullan-m <jmlindi@stud.ntnu.no>
2020-08-26 21:21:29 +0200

    new parsing method for newsatni

--- utilities.py
+++ utilities.py
@@ -1,5 +1,4 @@
-import urllib
-import urllib.request
+import requests
 import json
 
 UTF8_CHR = {'á': '%C3%A1',
@@ -13,6 +12,17 @@ UTF8_CHR = {'á': '%C3%A1',
             'ø': '%C3%B8'
             }
 
+headers = {
+    'Content-Type': 'application/json',
+}
+
+data = '{"operationName":"AllArticles","variables":{"lemma":"mann","wantedLangs":["sme","smj","smn","sms","fin","nob","swe","lat","eng","nno","sma"],"wantedDicts":["termwiki","gtsmenob","gtnobsme","gtnobsma","gtsmanob","gtsmefin","gtfinsme","gtsmesmn","gtsmnsme","sammallahtismefin","gtfinsmn","gtsmnfin"]},"query":"query AllArticles($lemma: String!, $wantedLangs: [String]!, $wantedDicts: [String]!) {\\n dictEntryList(exact: $lemma, wanted: $wantedLangs, wantedDicts: $wantedDicts) {\\n dictName\\n srcLang\\n targetLang\\n lookupLemmas {\\n edges {\\n node {\\n lemma\\n language\\n pos\\n __typename\\n }\\n __typename\\n }\\n __typename\\n }\\n translationGroups {\\n translationLemmas {\\n edges {\\n node {\\n lemma\\n language\\n pos\\n __typename\\n }\\n __typename\\n }\\n __typename\\n }\\n restriction {\\n restriction\\n attributes\\n __typename\\n }\\n exampleGroups {\\n example\\n translation\\n __typename\\n }\\n __typename\\n }\\n __typename\\n }\\n conceptList(exact: $lemma, wanted: $wantedLangs) {\\n name\\n collections\\n definition\\n explanation\\n terms {\\n note\\n source\\n status\\n expression {\\n lemma\\n language\\n pos\\n __typename\\n }\\n __typename\\n }\\n __typename\\n }\\n}\\n"}'
+
+response = requests.post(
+    'https://satni.uit.no/newsatni/', headers=headers, data=data)
+with open('data.json', 'w') as f:
+    json.dump(response.json(), f)
+
 
 def utf8ize(string):
     # Deprecated function, replaced by urllib.parse.quote()
@@ -23,36 +33,37 @@ def utf8ize(string):
 
 
 def web2json(url):
-    data = urllib.request.urlopen(url).read()
+    data = request.urlopen(url).read()
     if (data[0] == 123 and data[2] == 123):  # 123 in ASCII corresponds to {
         return [json.loads(data[1:len(data)-1])]
     return json.loads(data)
 
 
 def search(string):
-    perenc = urllib.parse.quote(string)  # utf8ize(string)
+    perenc = parse.quote(string)  # utf8ize(string)
     results = web2json(
-        'http://satni.uit.no:8080/exist/restxq/satni/search?query=' + perenc)
+        'https://satni.uit.no/newsatni/exist/restxq/satni/search?query=' + perenc)
     return results
 
 
 def get_w_article(term):
-    perenc = urllib.parse.quote(term)  # utf8ize(term)
+    perenc = parse.quote(term)  # utf8ize(term)
     article = web2json(
-        'http://satni.uit.no:8080/exist/restxq/satni/article/' + perenc)
+        'https://satni.uit.no/newsatni/exist/restxq/satni/article/' + perenc)
     return article
 
 
 def get_p_article(term, dictname, src_lang, dest_lang, pos=""):
-    perenc = urllib.parse.quote(term)
+    perenc = parse.quote(term)
     postxt = "" if pos else f"?pos={pos}"
-    data = urllib.request.urlopen(
+    data = request.urlopen(
         f"https://{dictname}.oahpa.no/paradigm/{src_lang}/{dest_lang}/{perenc}/{postxt}").read()
     return json.loads(data)
 
+
 def get_e_article(term, dictname, src_lang, dest_lang):
-    perenc = urllib.parse.quote(term)
-    data = urllib.request.urlopen(
+    perenc = parse.quote(term)
+    data = request.urlopen(
         f"https://{dictname}.oahpa.no/lookup/{src_lang}/{dest_lang}/?lookup={perenc}").read()
     return json.loads(data)
 
