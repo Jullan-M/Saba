8fb682529f6a99fc4f873cadc69d75ee88a1c91e
Jullan-M <jmihkkal_lindi@hotmail.com>
2020-09-19 01:28:08 +0200

    fix ssl error (sketchy)

--- utilities.py
+++ utilities.py
@@ -1,32 +1,14 @@
-import urllib
-import json
-
-def web2json(url):
-    data = urllib.request.urlopen(url).read()
-    if (data[0] == 123 and data[2] == 123):  # 123 in ASCII corresponds to {
-        return [json.loads(data[1:len(data)-1])]
-    return json.loads(data)
-
-
-def search(string):
-    perenc = urllib.parse.quote(string)  # utf8ize(string)
-    results = web2json(
-        'https://satni.uit.no:8080/exist/restxq/satni/search?query=' + perenc)
-    return results
+import requests
 
 def get_p_article(term, dictname, src_lang, dest_lang, pos=""):
-    perenc = urllib.parse.quote(term)
     postxt = "" if pos else f"?pos={pos}"
-    data = urllib.request.urlopen(
-        f"https://{dictname}.oahpa.no/paradigm/{src_lang}/{dest_lang}/{perenc}/{postxt}").read()
-    return json.loads(data)
+    data = requests.get(f"https://{dictname}.oahpa.no/paradigm/{src_lang}/{dest_lang}/{term}/{postxt}", verify=False)
+    return data.json()
 
 
 def get_e_article(term, dictname, src_lang, dest_lang):
-    perenc = urllib.parse.quote(term)
-    data = urllib.request.urlopen(
-        f"https://{dictname}.oahpa.no/lookup/{src_lang}/{dest_lang}/?lookup={perenc}").read()
-    return json.loads(data)
+    data = requests.get(f"https://{dictname}.oahpa.no/lookup/{src_lang}/{dest_lang}/?lookup={term}", verify=False)
+    return data.json()
 
 
 def waittime_between(time, hrs, mins, secs):
